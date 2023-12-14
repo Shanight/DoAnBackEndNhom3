50226e90491456ef70091d07e072c088a9f3c225
Phan Gia Huy <phangiahuy8033@gmail.com>
2023-12-14 14:40:42 +0700

    update

---
+++ HololiveSln/Hololive.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Hololive.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hololive.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<HololiveEntity> Users { get; set; }
+
+        public DbSet<Products> Products { get; set; }
+    }
+}
---
+++ HololiveSln/Hololive.Web/Migrations/20231213153845_InditityDatabaseSetup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hololive.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InditityDatabaseSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "varchar(255)", nullable: false),
+                    LastName = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Password = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Role = table.Column<string>(type: "varchar(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
