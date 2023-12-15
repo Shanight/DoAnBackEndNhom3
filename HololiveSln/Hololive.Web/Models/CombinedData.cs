267e93d941e77530253747f6aaf8fb4a136f724b
Phan Gia Huy <phangiahuy8033@gmail.com>
2023-12-15 10:38:54 +0700

    update dot 2

---
+++ HololiveSln/Hololive.Web/Models/CombinedData.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Hololive.Web.Models
+{
+    public class CombinedData
+    {
+        public List<Products> Products { get; set; }
+        public List<Event> Events { get; set; }
+    }
+}
