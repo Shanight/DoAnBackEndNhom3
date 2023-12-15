a4e30ce78c73da64048f5656fe2cb09842a473e7
Phan Gia Huy <phangiahuy8033@gmail.com>
2023-12-15 10:36:26 +0700

    update dot1

--- HololiveSln/Hololive.Web/Controllers/HomeController.cs
+++ HololiveSln/Hololive.Web/Controllers/HomeController.cs
@@ -1,23 +1,35 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Hololive.Web.Models;
+using Hololive.Web.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hololive.Web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _dbContext;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
     {
         _logger = logger;
+        _dbContext = dbContext;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
-    }
+        var productList = await _dbContext.Products.ToListAsync();
+        var eventList = await _dbContext.Events.ToListAsync();
 
+        var combinedData = new CombinedData
+        {
+            Products = productList,
+            Events = eventList
+        };
+
+        return View(combinedData);
+    }
     public IActionResult Privacy()
     {
         return View();
