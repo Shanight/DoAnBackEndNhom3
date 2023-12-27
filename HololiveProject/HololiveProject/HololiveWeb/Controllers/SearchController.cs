7c1916d749fa8eb9a72d7282aae50c4c42275de8
Phan Gia Huy <phangiahuy8033@gmail.com>
2023-12-27 22:30:04 +0700

    update

--- HololiveProject/HololiveProject/HololiveWeb/Controllers/SearchController.cs
+++ HololiveProject/HololiveProject/HololiveWeb/Controllers/SearchController.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using HololiveWeb.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+
 namespace HololiveWeb.Controllers
 {
     public class SearchController : Controller
@@ -13,11 +17,27 @@ namespace HololiveWeb.Controllers
         {
             _dbContext = dbContext;
         }
-        public async Task<IActionResult> Index()
-        {
-            var productList = await _dbContext.Products.ToListAsync();
 
+        public async Task<IActionResult> Index(string keyword, List<string> selectedCategories)
+        {
+            var query = _dbContext.Products.AsQueryable();
+
+            // Check and apply search keyword
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                query = query.Where(p => p.Name.Contains(keyword));
+            }
+
+            // Check and apply selected categories
+            if (selectedCategories != null && selectedCategories.Any())
+            {
+                query = query.Where(p => selectedCategories.Contains(p.Catetory));
+            }
+            
+
+            var productList = await query.ToListAsync();
             return View(productList);
         }
+
     }
 }
